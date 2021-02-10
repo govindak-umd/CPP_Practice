1bcd5fb21cb99fbb16c0da3976ac597f3b99c0e7
govindak-umd <Porsche_911GT3RS>
2021-02-10 15:14:17 -0500

    Functions to add nodes at the end and beginning added

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,12 +1,39 @@
 # include <iostream>
 using namespace std;
 
+/**
+ * Node Class
+ */
 class Node{
 public:
     int data;
     Node* next;
 };
 
+/**
+ * Printing nodes LinkedList
+ */
+void printLinkedList(Node*);
+
+/**
+ * Insert Node in between any two nodes
+ * @param pos
+ * @param data
+ */
+Node* insertNodeBeginning(int , Node*);
+
+/**
+ * Insert node at the end
+ * @param pos
+ * @param val
+ * @param test_node
+ */
+void insertNodeEnd(int , Node*);
+
+/**
+ * Main Function
+ * @return
+ */
 int main(){
     Node* first = NULL;
     Node* second = NULL;
@@ -26,5 +53,52 @@ int main(){
     third->data = 3;
     third->next = NULL;
 
+    printLinkedList(first);
+
+    Node* new_node = insertNodeBeginning(5,first);
+
+    printLinkedList(new_node);
+
+    insertNodeEnd(22, third);
+    printLinkedList(new_node);
     return 0;
+}
+
+/**
+ * Print LinkedList Function
+ * @param a
+ */
+void printLinkedList(Node* a){
+    while (a != NULL){
+        cout <<a-> data << " ";
+        a = a->next;
+    }
+    cout << endl;
+}
+
+/**
+ * Insert Node in between any two nodes
+ * @param pos
+ * @param data
+ */
+Node*  insertNodeBeginning(int val, Node* test_node){
+    Node* new_node = NULL;
+    new_node = new Node();
+    new_node->data = val ;
+    new_node->next = test_node;
+    return new_node;
+}
+
+/**
+ * Insert node at the end
+ * @param pos
+ * @param val
+ * @param test_node
+ */
+void insertNodeEnd(int val, Node* test_node){
+    Node* new_node = NULL;
+    new_node = new Node();
+    test_node->next = new_node;
+    new_node->data = val ;
+    new_node->next = NULL;
 }
