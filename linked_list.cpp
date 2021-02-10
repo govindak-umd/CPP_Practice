5147c1081046dbbd7c94be932d30a8955395ea27
Govind <govindajithkumar@gmail.com>
2021-02-10 14:26:25 -0500

    Linked List Added

---
+++ linked_list.cpp
@@ -0,0 +1,30 @@
+# include <iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+};
+
+int main(){
+    Node* first = NULL;
+    Node* second = NULL;
+    Node* third = NULL;
+
+    first = new Node();
+    second = new Node();
+    third = new Node();
+
+    first->data = 1;
+    first->next = second;
+
+
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    third->next = NULL;
+
+    return 0;
+}
