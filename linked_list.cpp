0fecadf6352be9558856a5859b5b071500a2c1f9
govindak-umd <Porsche_911GT3RS>
2021-02-10 15:57:15 -0500

    Added func to add in between

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,26 +1,27 @@
 # include <iostream>
+
 using namespace std;
 
 /**
  * Node Class
  */
-class Node{
+class Node {
 public:
     int data;
-    Node* next;
+    Node *next;
 };
 
 /**
  * Printing nodes LinkedList
  */
-void printLinkedList(Node*);
+void printLinkedList(Node *);
 
 /**
  * Insert Node in between any two nodes
  * @param pos
  * @param data
  */
-Node* insertNodeBeginning(int , Node*);
+Node *insertNodeBeginning(int, Node *);
 
 /**
  * Insert node at the end
@@ -28,16 +29,22 @@ Node* insertNodeBeginning(int , Node*);
  * @param val
  * @param test_node
  */
-void insertNodeEnd(int , Node*);
+void insertNodeEnd(int, Node *);
+
+/**
+ * Insert a node in the middle
+ * @return
+ */
+Node* insertNodeMiddle(int , Node*);
 
 /**
  * Main Function
  * @return
  */
-int main(){
-    Node* first = NULL;
-    Node* second = NULL;
-    Node* third = NULL;
+int main() {
+    Node *first = NULL;
+    Node *second = NULL;
+    Node *third = NULL;
 
     first = new Node();
     second = new Node();
@@ -55,11 +62,15 @@ int main(){
 
     printLinkedList(first);
 
-    Node* new_node = insertNodeBeginning(5,first);
+    Node *new_node = insertNodeBeginning(5, first);
 
     printLinkedList(new_node);
 
     insertNodeEnd(22, third);
+    printLinkedList(new_node);
+
+    insertNodeMiddle(59,second);
+
     printLinkedList(new_node);
     return 0;
 }
@@ -68,9 +79,9 @@ int main(){
  * Print LinkedList Function
  * @param a
  */
-void printLinkedList(Node* a){
-    while (a != NULL){
-        cout <<a-> data << " ";
+void printLinkedList(Node *a) {
+    while (a != NULL) {
+        cout << a->data << " ";
         a = a->next;
     }
     cout << endl;
@@ -81,10 +92,10 @@ void printLinkedList(Node* a){
  * @param pos
  * @param data
  */
-Node*  insertNodeBeginning(int val, Node* test_node){
-    Node* new_node = NULL;
+Node* insertNodeBeginning(int val, Node *test_node) {
+    Node *new_node = NULL;
     new_node = new Node();
-    new_node->data = val ;
+    new_node->data = val;
     new_node->next = test_node;
     return new_node;
 }
@@ -95,10 +106,19 @@ Node*  insertNodeBeginning(int val, Node* test_node){
  * @param val
  * @param test_node
  */
-void insertNodeEnd(int val, Node* test_node){
-    Node* new_node = NULL;
+void insertNodeEnd(int val, Node *test_node) {
+    Node *new_node = NULL;
     new_node = new Node();
     test_node->next = new_node;
-    new_node->data = val ;
+    new_node->data = val;
     new_node->next = NULL;
+}
+
+Node* insertNodeMiddle(int val, Node *test_node){
+    Node* new_node= NULL;
+    new_node = new Node();
+    new_node->next = test_node->next;
+    test_node->next = new_node;
+    new_node->data = val;
+    return test_node;
 }
