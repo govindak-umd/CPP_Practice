e0194bbc490785c193189632ddfdf4e344851782
govindak-umd <Porsche_911GT3RS>
2021-02-11 07:28:10 -0500

    Pointer Practice

---
+++ cmake-build-debug/CMakeFiles/pointer_practice.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pointer_practice.dir/pointer_practice.cpp.o"
+  "pointer_practice"
+  "pointer_practice.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pointer_practice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
