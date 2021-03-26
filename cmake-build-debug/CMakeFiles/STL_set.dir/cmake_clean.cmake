3114bf61afc0666f29dcb07ab40899971abd7c39
Govind Ajith Kumar <50176400+govindak-umd@users.noreply.github.com>
2021-03-26 09:53:20 -0400

    Sets C++

---
+++ cmake-build-debug/CMakeFiles/STL_set.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/STL_set.dir/STL_set.cpp.obj"
+  "STL_set.exe"
+  "STL_set.exe.manifest"
+  "STL_set.pdb"
+  "libSTL_set.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/STL_set.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
