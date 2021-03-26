bc13e25f0f38060bde78e6b687659ae46d1bcb1b
Govind Ajith Kumar <50176400+govindak-umd@users.noreply.github.com>
2021-03-26 13:40:23 -0400

    Queue C++

---
+++ cmake-build-debug/CMakeFiles/STL_queue.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/STL_queue.dir/STL_queue.cpp.obj"
+  "STL_queue.exe"
+  "STL_queue.exe.manifest"
+  "STL_queue.pdb"
+  "libSTL_queue.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/STL_queue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
